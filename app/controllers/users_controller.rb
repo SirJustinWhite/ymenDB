8060f9e9b4a863bd2da4767bb5f975c8fc9e83b7
SirJustinWhite <Jjustinwhite@mac.com>
2014-05-19 19:30:21 -0500

    warning message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 
   # GET /users
   # GET /users.json
@@ -77,5 +79,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        flash[:warning] = "Please sign in."
+        redirect_to signin_url
+      end
     end
 end
